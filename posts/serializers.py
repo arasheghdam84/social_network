2cd3014f85266efa5946b5216cc9ca728d479b97
arthur_harington <arthur@ezpin.com>
2022-08-28 15:56:15 +0430

    Like and Comment added.

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Post
+from .models import Post, Comment, Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -10,3 +10,25 @@ class PostSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'user': {'read_only': True}
         }
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('post', 'user', 'text')
+        extra_kwargs = {
+            'post': {'read_only': True},
+            'user': {'read_only': True}
+        }
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = ('post', 'user', 'is_liked')
+        extra_kwargs = {
+            'post': {'read_only': True},
+            'user': {'read_only': True},
+            'is_liked': {'required': False}
+        }
+
